c6260fd08c80d373493e3adbb42a2f9a14781315
Franco Ortega <francopdx@gmail.com>
2021-03-13 10:48:42 -0800

    add Continent component with initial styling

---
+++ src/components/app/App.js
@@ -0,0 +1,43 @@
+import React from 'react'
+// import './App.css';
+import Continent from '../continents/Continent'
+
+function App() {
+  return (
+    <>
+      The Map to Land a Job
+      <Continent />
+      <Continent />
+      <Continent />
+      <Continent />
+    </>
+  )
+}
+
+export default App
+
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/components/continents/Continent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// import styles from './Continent.css';
+import styles from './Continent.module.css';
+
+function Continent() {
+    return (
+        <div className={styles.Continent}>
+            <h1 style={{color: 'green'}}>Continent!!!</h1>
+        </div>
+    )
+}
+
+export default Continent
