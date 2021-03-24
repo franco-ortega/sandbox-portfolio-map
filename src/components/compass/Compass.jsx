93fc9cc26c39c633d8d233eaeaf163ceb97e3aa3
Franco Ortega <francopdx@gmail.com>
2021-03-23 23:24:51 -0700

    comment out Compass circles

--- src/components/compass/Compass.jsx
+++ src/components/compass/Compass.jsx
@@ -4,9 +4,9 @@ import styles from './Compass.module.css';
 const Compass = () => {
     return (
         <div className={styles.Compass}>
-            <div className={styles.CompassRing}>
+            {/* <div className={styles.CompassRing}>
                 <div className={styles.CompassCenter}>
-                    <div className={styles.CompassDot}>
+                    <div className={styles.CompassDot}> */}
 
                     <hr className={styles.test3} />
                     <hr className={styles.test1} />
@@ -18,9 +18,9 @@ const Compass = () => {
                     <hr className={styles.test2} />
 
                     </div>
-                </div>
-            </div>
-        </div>
+        //         </div>
+        //     </div>
+        // </div>
     );
 };
 
