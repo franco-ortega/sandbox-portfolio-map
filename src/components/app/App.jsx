d39443fd23d115c77aa426aa13b31674610b5e46
Franco Ortega <francopdx@gmail.com>
2021-03-28 16:54:42 -0700

    add media queries to force landscape orientation on screens smaller than 900px

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import Ocean from '../oceans/Ocean';
 // import Compass from '../compass/Compass';
+import styles from './App.module.css'
 
 const App = () => {
   return (
-    <>
+    <div className={styles.App}>
       {/* <Compass /> */}
       <Ocean />      
-    </>
+    </div>
   );
 };
 
